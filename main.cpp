d63c27708de12d8854e0a2a420636b376e9952aa
RaymondMa <raymond.ma.cn@gmail.com>
2014-12-13 20:02:36 +0800

    Create main.cpp
    Update Source Code for HelloWorldGit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/** Test Function for Visual Studio 2012
+ *  @Output print "Hello World!"
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    cout << "Hello World!" << endl;
+
+    while (1);
+}
